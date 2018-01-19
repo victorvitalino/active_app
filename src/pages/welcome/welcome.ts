5e03f16c5cd685e1948ea1d6445db6ab76a37c18
Victor Vitalino <vitalino.victor@gmail.com>
2018-01-19 10:26:11 -0200

    Fix data-services merge

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import HomePage from '../home/home'
-/**
- * Generated class for the WelcomePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -24,6 +18,6 @@ export class WelcomePage {
   }
 
   goToHome() {
-    this.navCtrl.push(HomePage);
+    this.navCtrl.push('HomePage');
   }
 }
